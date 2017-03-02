a8038a9d4dc406654987f7a1cbd4fe8e064b437c
racheltstevens <racheltstevens@gmail.com>
2017-03-02 13:06:07 -0500

    Add example scripts
    For reference during lesson.

--- scripts/examples-create.sh
+++ scripts/examples-create.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+API="http://localhost:4741"
+URL_PATH="/examples"
+
+curl --include --request POST "${API}${URL_PATH}" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "example": {
+      "text": "My first example text"
+    }
+  }'
+
+echo
