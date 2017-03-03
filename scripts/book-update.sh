af229f25502168c448749e9f3722a6f3b0e0bd09
laurpaik <laurpaik@gmail.com>
2017-03-03 12:40:09 -0500

    Write script to update book title and author

--- scripts/book-update.sh
+++ scripts/book-update.sh
@@ -1,1 +1,18 @@
 #!/bin/sh
+
+API="http://localhost:4741"
+URL_PATH="/books"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "book": {
+      "title": "'"${TITLE}"'",
+      "author": "'"${AUTHOR}"'"
+    }
+  }'
+
+echo
