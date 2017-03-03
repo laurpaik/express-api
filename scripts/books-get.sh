60dbc4e04a1d3e6a206c9fe90da450e916f6ef75
laurpaik <laurpaik@gmail.com>
2017-03-03 11:05:45 -0500

    Write script to get books

--- scripts/books-get.sh
+++ scripts/books-get.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+# Things to change when copying:
+# URL_PATH
+# Decide whether or not we need a token
+# 404 errors are almost always about routes fyi LOL
+API="http://localhost:4741"
+URL_PATH="/books"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request GET
+
+echo
