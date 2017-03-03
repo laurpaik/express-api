20e2c959a6234c48bee9248ed8f4446bffb75a54
laurpaik <laurpaik@gmail.com>
2017-03-03 11:17:41 -0500

    Write model for book

--- app/models/book.js
+++ app/models/book.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      let userId = (options.user && options.user._id) || false;
+      ret.editable = userId && userId.equals(doc._owner);
+      return ret;
+    },
+  },
+});
+
+bookSchema.virtual('length').get(function length() {
+  return this.text.length;
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
