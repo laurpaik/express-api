55ca73cdac904827b0276798cf9a7e93b3a7f4b2
laurpaik <laurpaik@gmail.com>
2017-03-03 11:16:42 -0500

    Write index controller for books

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -1,1 +1,26 @@
 'use strict';
+const controller = require('lib/wiring/controller');
+const models = require('app/models');
+const Book = models.book;
+
+const authenticate = require('./concerns/authenticate');
+const setUser = require('./concerns/set-current-user');
+const setModel = require('./concerns/set-mongoose-model');
+
+const index = (req, res, next) => {
+  Book.find()
+    .then(books => res.json({
+      books: books.map((book) =>
+        book.toJSON({ virtuals: true, user: req.user })),
+    }))
+    .catch(next);
+};
+
+module.exports = controller({
+  index,
+}, { before: [
+  { method: setUser, only: ['index', 'show'] },
+  { method: authenticate, except: ['index', 'show'] },
+  { method: setModel(Book), only: ['show'] },
+  { method: setModel(Book, { forUser: true }), only: ['update', 'destroy'] },
+], });
