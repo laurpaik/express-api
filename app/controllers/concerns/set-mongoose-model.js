7e621db368cc68fe81b5f23ddae6ddfb91e58385
laurpaik <laurpaik@gmail.com>
2017-03-03 11:48:18 -0500

    Annotate set mongoose model
    - this file is the reason we don't have to find the example in the
    controller
    - it handles any errors

--- app/controllers/concerns/set-mongoose-model.js
+++ app/controllers/concerns/set-mongoose-model.js
@@ -2,6 +2,8 @@
 
 const HttpError = require('lib/wiring/errors/http-error');
 
+// looks for a specific example (or book)
+// sets that on the request
 const setMongooseModel = (model, options) =>
   function (req, res, next) {
     let search = { _id: req.params.id };
@@ -9,12 +11,15 @@ const setMongooseModel = (model, options) =>
       search._owner = req.user;
     }
 
+    // in our case, this will be Example.findOne()
     model.findOne(search, (error, document) => {
       error = error || !document && new HttpError(404);
       if (error) {
         return next(error);
       }
 
+      // if there's no error getting the example from the database
+      // it sets req.example to the mongoose document it found
       req[model.modelName.toLowerCase()] = document;
       next();
     });
