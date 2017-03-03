8e649f2281721cd29c6d0cb90192801cba4c4594
laurpaik <laurpaik@gmail.com>
2017-03-03 12:55:07 -0500

    Write script to create a book

--- scripts/books-create.sh
+++ scripts/books-create.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+API="http://localhost:4741"
+URL_PATH="/books"
+TOKEN="8EwGkVN8zMRVuMVhmDXHByKhON802fCPFk7e59KewRE=--D4k8591O06JXcgMYoG+j7N7NvYqTDDVoKuIqTJBQo7Q="
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "book": {
+      "title": "'"${TITLE}"'",
+      "author": "'"${AUTHOR}"'",
+      "originalLanguage": "'"${LANGUAGE}"'",
+      "firstPublished": "'"${PUBLISHED}"'"
+    }
+  }'
+
+echo
